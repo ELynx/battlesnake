2d961ba8b6d87d9c5e00718c322ed51c33ed408c
ELynx <1621251+ELynx@users.noreply.github.com>
2020-03-12 14:17:10 +0300

    Proper type assignment for GameController

--- src/main/java/ru/elynx/battlesnake/webserver/GameController.java
+++ src/main/java/ru/elynx/battlesnake/webserver/GameController.java
@@ -1,37 +1,43 @@
 package ru.elynx.battlesnake.webserver;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.elynx.battlesnake.protocol.GameState;
 import ru.elynx.battlesnake.protocol.Move;
 import ru.elynx.battlesnake.protocol.SnakeConfig;
 
+import java.io.InvalidObjectException;
+
 @RestController
 public class GameController {
-    static private Move hardcode = new Move("up", "2% ready");
+    static private Move hardcode = new Move("up", "3% ready");
 
-    @PostMapping("/start")
-    public ResponseEntity<SnakeConfig> start(GameState gameState) {
+    private static void ValidateGameState(GameState gameState) throws InvalidObjectException {
         if (GameState.isInvalid(gameState))
-            return ResponseEntity.badRequest().build();
+            throw new InvalidObjectException("Provided GameState is not valid");
+    }
 
+    @ExceptionHandler(InvalidObjectException.class)
+    public ResponseEntity<String> handleException(InvalidObjectException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @PostMapping(path = "/start", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SnakeConfig> start(@RequestBody GameState gameState) throws InvalidObjectException {
+        ValidateGameState(gameState);
         return ResponseEntity.ok(SnakeConfig.DEFAULT_SNAKE_CONFIG);
     }
 
-    @PostMapping("/move")
-    public ResponseEntity<Move> move(GameState gameState) {
-        if (GameState.isInvalid(gameState))
-            return ResponseEntity.badRequest().build();
-
+    @PostMapping(path = "/move", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Move> move(@RequestBody GameState gameState) throws InvalidObjectException {
+        ValidateGameState(gameState);
         return ResponseEntity.ok(hardcode);
     }
 
-    @PostMapping("/end")
-    public ResponseEntity<Void> end(GameState gameState) {
-        if (GameState.isInvalid(gameState))
-            return ResponseEntity.badRequest().build();
-
+    @PostMapping(path = "/end", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> end(@RequestBody GameState gameState) throws InvalidObjectException{
+        ValidateGameState(gameState);
         return ResponseEntity.ok().build();
     }
 
--- src/test/java/ru/elynx/battlesnake/BattlesnakeApplicationTests.java
+++ src/test/java/ru/elynx/battlesnake/BattlesnakeApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -63,23 +67,21 @@ class BattlesnakeApplicationTests {
 
     @Test
     public void startHasColor() throws Exception {
-        mockMvc.perform(post("/start").content(ApiExampleGameState))
-                .andDo(print())
+        mockMvc.perform(post("/start").content(ApiExampleGameState).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("color")));
     }
 
     @Test
     public void moveHasMove() throws Exception {
-        mockMvc.perform(post("/move").content(ApiExampleGameState))
-                .andDo(print())
+        mockMvc.perform(post("/move").content(ApiExampleGameState).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("move")));
     }
 
     @Test
     public void endIsOk() throws Exception {
-        mockMvc.perform(post("/end").content(ApiExampleGameState))
+        mockMvc.perform(post("/end").content(ApiExampleGameState).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
@@ -91,7 +93,22 @@ class BattlesnakeApplicationTests {
 
     @Test
     public void invalidInputNotOk() throws Exception {
-        mockMvc.perform(post("/start").content("No carrier"))
-                .andExpect(status().isBadRequest());
+        List<String> urls = new LinkedList<>();
+        urls.add("/start");
+        urls.add("/move");
+        urls.add("/end");
+
+        List<String> contents = new LinkedList<>();
+        contents.add("No carrier");
+        contents.add("{ No carrier }");
+        contents.add("{ \"no\" : \"carrier\" }");
+
+
+        for (String url : urls) {
+            for (String content : contents) {
+                mockMvc.perform(post(url).content(content).contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isBadRequest());
+            }
+        }
     }
 }
