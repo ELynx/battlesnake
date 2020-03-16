3d0077bba1932ec1717aa9bcae09ad2ba5c1edbc
ELynx <1621251+ELynx@users.noreply.github.com>
2020-03-16 18:54:39 +0300

    attempt basic wall avoidance

--- src/main/java/ru/elynx/battlesnake/engine/GameEngine.java
+++ src/main/java/ru/elynx/battlesnake/engine/GameEngine.java
@@ -1,11 +1,16 @@
 package ru.elynx.battlesnake.engine;
 
+import ru.elynx.battlesnake.protocol.Coords;
 import ru.elynx.battlesnake.protocol.GameState;
 import ru.elynx.battlesnake.protocol.Move;
 import ru.elynx.battlesnake.protocol.SnakeConfig;
 
 public class GameEngine implements IGameEngine {
-    static private Move hardcode = new Move("up", "3% ready");
+    private static String UP = "up";
+    private static String RIGHT = "right";
+    private static String DOWN = "down";
+    private static String LEFT = "left";
+    String move = UP;
 
     @Override
     public SnakeConfig processStart(GameState gameState) {
@@ -14,7 +19,23 @@ public class GameEngine implements IGameEngine {
 
     @Override
     public Move processMove(GameState gameState) {
-        return hardcode;
+        int width = gameState.getBoard().getWidth();
+        int height = gameState.getBoard().getHeight();
+
+        Coords head = gameState.getYou().getBody().get(0);
+        int x = head.getX();
+        int y = head.getY();
+
+        if (y == 0 && UP.equals(move))
+            move = RIGHT;
+        else if (x == width - 1 && RIGHT.equals(move))
+            move = DOWN;
+        else if (y == height - 1 && DOWN.equals(move))
+            move = LEFT;
+        else if (x == 0 && LEFT.equals(move))
+            move = UP;
+
+        return new Move(move, "4% ready");
     }
 
     @Override
