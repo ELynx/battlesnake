4153d4ae1257332683c1b9161c1690057cac4711
ELynx <1621251+ELynx@users.noreply.github.com>
2020-03-21 17:53:26 +0300

    basic weighting

--- src/main/java/ru/elynx/battlesnake/engine/GameEngine.java
+++ src/main/java/ru/elynx/battlesnake/engine/GameEngine.java
@@ -1,50 +1,104 @@
 package ru.elynx.battlesnake.engine;
 
-import ru.elynx.battlesnake.protocol.Coords;
-import ru.elynx.battlesnake.protocol.GameState;
-import ru.elynx.battlesnake.protocol.Move;
-import ru.elynx.battlesnake.protocol.SnakeConfig;
+import ru.elynx.battlesnake.engine.math.Matrix;
+import ru.elynx.battlesnake.protocol.*;
+
+import java.util.List;
 
 public class GameEngine implements IGameEngine {
-    private static String UP = "up";
-    private static String RIGHT = "right";
-    private static String DOWN = "down";
-    private static String LEFT = "left";
+    private final static String UP = "up";
+    private final static String RIGHT = "right";
+    private final static String DOWN = "down";
+    private final static String LEFT = "left";
+
+    protected boolean initialized = false;
+    Matrix matrix;
+    int maxHealth;
+
+    protected void initOnce(GameState gameState) {
+        if (initialized)
+            return;
+
+        matrix = Matrix.zeroMatrix(gameState.getBoard().getWidth(), gameState.getBoard().getHeight());
+        maxHealth = gameState.getYou().getHealth();
+    }
 
     @Override
     public SnakeConfig processStart(GameState gameState) {
+        initOnce(gameState);
         return getSnakeConfig();
+    }
+
+    protected void applyGameState(GameState gameState) {
+        int ownSize = gameState.getYou().getBody().size();
+        int ownHealth = gameState.getYou().getHealth();
+
+        // apply snake bodies for collision
+        for (Snake snake : gameState.getBoard().getSnakes()) {
+            List<Coords> body = snake.getBody();
+            for (int i = 0, size = body.size(); i < size; ++i) {
+                Integer x = body.get(i).getX();
+                Integer y = body.get(i).getY();
+
+                // since we are looking for strictly less own body will get into wall category
+                if (i == 0 && size < ownSize)
+                    matrix.splash1stOrder(x, y, 0.75); // TODO as parameter
+                else
+                    matrix.setValue(x, y, -1.0d); // avoid
+            }
+        }
+
+        if (ownHealth < maxHealth) {
+            double foodWeight = 1.0d - (double) ownHealth / (double) maxHealth;
+
+            for (Coords food : gameState.getBoard().getFood()) {
+                Integer x = food.getX();
+                Integer y = food.getY();
+
+                matrix.splash2ndOrder(x, y, foodWeight);
+            }
+        }
+    }
+
+    protected String bestMove(Coords head) {
+        Integer x = head.getX();
+        Integer y = head.getY();
+
+        // index ascending order
+
+        String bestDirection = UP;
+        double bestValue = matrix.getValue(x, y - 1);
+
+        double nextValue = matrix.getValue(x - 1, y);
+        if (nextValue > bestValue) {
+            bestDirection = LEFT;
+            bestValue = nextValue;
+        }
+
+        nextValue = matrix.getValue(x + 1, y);
+        if (nextValue > bestValue) {
+            bestDirection = RIGHT;
+            bestValue = nextValue;
+        }
+
+        nextValue = matrix.getValue(x, y + 1);
+        if (nextValue > bestValue) {
+            bestDirection = DOWN;
+            //bestValue = nextValue;
+        }
+
+        return bestDirection;
+    }
+
+    protected String makeMove(GameState gameState) {
+        initOnce(gameState);
+        applyGameState(gameState);
+        return bestMove(gameState.getYou().getBody().get(0));
     }
 
     @Override
     public Move processMove(GameState gameState) {
-        int width = gameState.getBoard().getWidth();
-        int height = gameState.getBoard().getHeight();
-
-        Coords head = gameState.getYou().getBody().get(0);
-        int x = head.getX();
-        int y = head.getY();
-
-        String move = UP;
-
-        if (y == 0)
-            move = RIGHT;
-
-        if (x == width - 1)
-            move = DOWN;
-
-        if (y == height - 1)
-            move = LEFT;
-
-        if (x == 0) {
-            if (y > 0) {
-                move = UP;
-            } else {
-                // the only "edge case to stateless logic"
-                move = RIGHT;
-            }
-        }
-
+        String move = makeMove(gameState);
         return new Move(move, "4% ready");
     }
 
---
+++ src/main/java/ru/elynx/battlesnake/engine/math/Matrix.java
@@ -0,0 +1,103 @@
+package ru.elynx.battlesnake.engine.math;
+
+public class Matrix {
+    private final int width;
+    private final int height;
+    private final double[] values;
+
+    protected Matrix(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.values = new double[width * height];
+    }
+
+    public static Matrix zeroMatrix(int width, int height) {
+        return new Matrix(width, height);
+    }
+
+    public double getValue(int x, int y) {
+        int index = safeIndex(x, y);
+        if (index < 0)
+            return -1.0d; //TODO theory
+
+        return unsafeGetValue(index);
+    }
+
+    public void setValue(int x, int y, double value) {
+        int index = safeIndex(x, y);
+        if (index < 0)
+            return;
+
+        unsafeSetValue(index, value);
+    }
+
+    protected void addValue(int x, int y, double value) {
+        int index = safeIndex(x, y);
+        if (index < 0)
+            return;
+
+        unsafeAddValue(index, value);
+    }
+
+    public boolean splash1stOrder(int x, int y, double valueAtImpact) {
+        // no impact - no setter
+        if (valueAtImpact == 0.0d)
+            return false;
+
+        // TODO can be optimized
+
+        // if impact is out of matrix ignore the setter
+        int index = safeIndex(x, y);
+        if (index < 0)
+            return false;
+
+        unsafeAddValue(index, valueAtImpact);
+
+        valueAtImpact = valueAtImpact / 2.0d;
+
+        addValue(x, y - 1, valueAtImpact);
+        addValue(x - 1, y, valueAtImpact);
+        addValue(x + 1, y, valueAtImpact);
+        addValue(x, y + 1, valueAtImpact);
+
+        return true;
+    }
+
+    public boolean splash2ndOrder(int x, int y, double valueAtImpact) {
+        // TODO can be optimized af
+
+        if (splash1stOrder(x, y, valueAtImpact)) {
+            valueAtImpact = valueAtImpact / 4.0d;
+
+            addValue(x - 1, y - 1, valueAtImpact);
+            addValue(x + 1, y - 1, valueAtImpact);
+            addValue(x - 1, y + 1, valueAtImpact);
+            addValue(x + 1, y + 1, valueAtImpact);
+        }
+
+        return false;
+    }
+
+    protected int unsafeIndex(int x, int y) {
+        return x + width * y;
+    }
+
+    protected int safeIndex(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height)
+            return -1;
+
+        return unsafeIndex(x, y);
+    }
+
+    protected double unsafeGetValue(int index) {
+        return values[index];
+    }
+
+    protected void unsafeSetValue(int index, double value) {
+        values[index] = value;
+    }
+
+    protected void unsafeAddValue(int index, double value) {
+        values[index] += value;
+    }
+}
