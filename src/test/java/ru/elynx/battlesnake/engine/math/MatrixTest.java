652ec7ba9d9cf4cf43c98bf6472165342c196161
ELynx <1621251+ELynx@users.noreply.github.com>
2020-03-21 18:34:16 +0300

    unit test Matrix

---
+++ src/test/java/ru/elynx/battlesnake/engine/math/MatrixTest.java
@@ -0,0 +1,86 @@
+package ru.elynx.battlesnake.engine.math;
+
+import org.junit.jupiter.api.Test;
+
+public class MatrixTest {
+    @Test
+    public void zeroMatrix() {
+        final int w = 11, h = 15;
+
+        Matrix matrix = Matrix.zeroMatrix(w, h);
+
+        for (int x = 0; x < w; ++x) {
+            for (int y = 0; y < h; ++y) {
+                assert (0.0d == matrix.getValue(x, y));
+            }
+        }
+    }
+
+    @Test
+    public void getSetZero() {
+        final int w = 11, h = 15;
+        final double v = 123.4;
+
+        Matrix matrix = Matrix.zeroMatrix(w, h);
+
+        for (int x = -1; x < w + 1; ++x) {
+            for (int y = -1; y < h + 1; ++y) {
+                matrix.setValue(x, y, v);
+
+                double v2 = matrix.getValue(x, y);
+
+                assert (v2 == v || v2 == -1.0d); // TODO indexes
+            }
+        }
+
+        // TODO as separate test
+        matrix.zero();
+
+        for (int x = 0; x < w; ++x) {
+            for (int y = 0; y < h; ++y) {
+                assert (0.0d == matrix.getValue(x, y));
+            }
+        }
+    }
+
+    @Test
+    public void splash1stOrder() {
+        final double v = 1.0d;
+
+        Matrix matrix = Matrix.zeroMatrix(4, 4);
+
+        assert (!matrix.splash1stOrder(-1, -1, v));
+        assert (!matrix.splash1stOrder(4, 4, v));
+        assert (matrix.splash1stOrder(1, 1, v));
+
+        assert (v == matrix.getValue(1, 1));
+
+        assert (v / 2.0d == matrix.getValue(1, 0));
+        assert (v / 2.0d == matrix.getValue(0, 1));
+        assert (v / 2.0d == matrix.getValue(2, 1));
+        assert (v / 2.0d == matrix.getValue(1, 2));
+    }
+
+    @Test
+    public void splash2ndOrder() {
+        final double v = 1.0d;
+
+        Matrix matrix = Matrix.zeroMatrix(4, 4);
+
+        assert (!matrix.splash2ndOrder(-1, -1, v));
+        assert (!matrix.splash2ndOrder(4, 4, v));
+        assert (matrix.splash2ndOrder(1, 1, v));
+
+        assert (v == matrix.getValue(1, 1));
+
+        assert (v / 2.0d == matrix.getValue(1, 0));
+        assert (v / 2.0d == matrix.getValue(0, 1));
+        assert (v / 2.0d == matrix.getValue(2, 1));
+        assert (v / 2.0d == matrix.getValue(1, 2));
+
+        assert (v / 4.0d == matrix.getValue(0, 0));
+        assert (v / 4.0d == matrix.getValue(0, 2));
+        assert (v / 4.0d == matrix.getValue(2, 0));
+        assert (v / 4.0d == matrix.getValue(2, 2));
+    }
+}
