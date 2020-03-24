e343901375b6cb8cadf73e2dd0aa5cff995407b9
ELynx <1621251+ELynx@users.noreply.github.com>
2020-03-24 11:13:34 +0300

    add new test case

--- src/test/java/ru/elynx/battlesnake/engine/GameEngineCaseTest.java
+++ src/test/java/ru/elynx/battlesnake/engine/GameEngineCaseTest.java
@@ -65,4 +65,76 @@ public class GameEngineCaseTest {
 
         assert (!"up".equalsIgnoreCase(moveMinHealth.getMove()));
     }
+
+    @Test
+    public void emptySpaceBetterThanSnake() throws Exception {
+        // https://play.battlesnake.com/g/646c44cd-c6f0-4a3f-ba7e-55357d0303cb/
+        // wrong decision at turn 49
+
+        IGameEngine gameEngine = gameEngineFactory.makeGameEngine();
+
+        GameState turn49 = new GameState();
+
+        turn49.setGame(new Game());
+        turn49.getGame().setId("646c44cd-c6f0-4a3f-ba7e-55357d0303cb");
+
+        turn49.setTurn(49);
+
+        turn49.setBoard(new Board());
+        turn49.getBoard().setWidth(11);
+        turn49.getBoard().setHeight(11);
+
+        turn49.getBoard().setFood(new LinkedList<>());
+
+        turn49.setYou(new Snake());
+        turn49.getYou().setId("qwerty");
+        turn49.getYou().setName("qwerty");
+        turn49.getYou().setHealth(100);
+        turn49.getYou().setBody(new LinkedList<>());
+        turn49.getYou().getBody().add(new Coords(7, 2));
+        turn49.getYou().getBody().add(new Coords(7, 1));
+        turn49.getYou().getBody().add(new Coords(8, 1));
+        turn49.getYou().getBody().add(new Coords(9, 1));
+        turn49.getYou().getBody().add(new Coords(9, 2));
+        turn49.getYou().getBody().add(new Coords(9, 3));
+        turn49.getYou().getBody().add(new Coords(8, 3));
+        turn49.getYou().setShout("qwerty");
+
+        turn49.getBoard().setSnakes(new LinkedList<>());
+        turn49.getBoard().getSnakes().add(turn49.getYou());
+
+        turn49.getBoard().getSnakes().add(new Snake());
+        turn49.getBoard().getSnakes().get(1).setId("enemy 1");
+        turn49.getBoard().getSnakes().get(1).setName("enemy 1");
+        turn49.getBoard().getSnakes().get(1).setHealth(100);
+        turn49.getBoard().getSnakes().get(1).setBody(new LinkedList<>());
+        turn49.getBoard().getSnakes().get(1).getBody().add(new Coords(3, 0));
+        turn49.getBoard().getSnakes().get(1).getBody().add(new Coords(3, 1));
+        turn49.getBoard().getSnakes().get(1).getBody().add(new Coords(4, 1));
+        turn49.getBoard().getSnakes().get(1).getBody().add(new Coords(5, 1));
+        turn49.getBoard().getSnakes().get(1).setShout("enemy 1");
+
+        turn49.getBoard().getSnakes().add(new Snake());
+        turn49.getBoard().getSnakes().get(2).setId("enemy 2");
+        turn49.getBoard().getSnakes().get(2).setName("enemy 2");
+        turn49.getBoard().getSnakes().get(2).setHealth(100);
+        turn49.getBoard().getSnakes().get(2).setBody(new LinkedList<>());
+        turn49.getBoard().getSnakes().get(2).getBody().add(new Coords(5, 2));
+        turn49.getBoard().getSnakes().get(2).getBody().add(new Coords(6, 2));
+        turn49.getBoard().getSnakes().get(2).getBody().add(new Coords(6, 3));
+        turn49.getBoard().getSnakes().get(2).getBody().add(new Coords(6, 4));
+        turn49.getBoard().getSnakes().get(2).getBody().add(new Coords(5, 4));
+        turn49.getBoard().getSnakes().get(2).getBody().add(new Coords(4, 4));
+        turn49.getBoard().getSnakes().get(2).setShout("enemy 2");
+
+        Move moveMaxHealth = gameEngine.processMove(turn49);
+
+        assert ("down".equalsIgnoreCase(moveMaxHealth.getMove()));
+
+        turn49.getYou().setHealth(0);
+
+        Move moveMinHealth = gameEngine.processMove(turn49);
+
+        assert ("down".equalsIgnoreCase(moveMinHealth.getMove()));
+    }
 }
