beebdd03484ad7fc196617e29a6299122be70d32
ELynx <1621251+ELynx@users.noreply.github.com>
2020-04-19 00:57:53 +0300

    lint

--- src/test/java/ru/elynx/battlesnake/webserver/GameControllerTest.java
+++ src/test/java/ru/elynx/battlesnake/webserver/GameControllerTest.java
@@ -1,20 +1,16 @@
 package ru.elynx.battlesnake.webserver;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.elynx.battlesnake.protocol.GameStateDto;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
