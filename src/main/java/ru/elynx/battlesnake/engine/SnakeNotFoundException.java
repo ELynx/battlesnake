6b1c8bb8c17bd419c92ca5c97fa298a14a5c3a40
ELynx <1621251+ELynx@users.noreply.github.com>
2021-03-10 17:59:37 +0300

    lint on lint

--- src/main/java/ru/elynx/battlesnake/engine/SnakeNotFoundException.java
+++ src/main/java/ru/elynx/battlesnake/engine/SnakeNotFoundException.java
@@ -1,7 +1,6 @@
 package ru.elynx.battlesnake.engine;
 
 public class SnakeNotFoundException extends IllegalArgumentException {
-
     public SnakeNotFoundException(String snakeName) {
         super("Game strategy [" + snakeName + "] is not registered");
     }
