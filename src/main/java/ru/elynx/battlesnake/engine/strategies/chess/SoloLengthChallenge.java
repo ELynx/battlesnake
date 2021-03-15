caeb135440965de14a2390e6db56012ef7bfc28b
ELynx <1621251+ELynx@users.noreply.github.com>
2021-03-15 17:23:16 +0300

    first "complete" implementation

--- src/main/java/ru/elynx/battlesnake/engine/strategies/chess/SoloLengthChallenge.java
+++ src/main/java/ru/elynx/battlesnake/engine/strategies/chess/SoloLengthChallenge.java
@@ -1,8 +1,9 @@
 package ru.elynx.battlesnake.engine.strategies.chess;
 
+import static ru.elynx.battlesnake.protocol.Move.Moves.*;
+
 import java.util.ArrayList;
 import ru.elynx.battlesnake.protocol.GameStateDto;
-import ru.elynx.battlesnake.protocol.Move;
 
 class SoloLengthChallenge extends ChessStrategy {
     @Override
@@ -26,12 +27,16 @@ class SoloLengthChallenge extends ChessStrategy {
 
     @Override
     protected StringField makeDirections(int stage, int width, int height) {
+        if (stage == 1)
+            return makeStage1(width, height);
+
+        // in any unknown situation, go loop
         return makeStage0(width, height);
     }
 
     /**
-     * Stage zero Loop at the top of the board until head follows tail Loop at the
-     * top of the board until whole board is food
+     * Stage zero. Loop at the bottom of the board until head follows tail. Loop at
+     * the bottom of the board until whole board is food.
      *
      * @param width
      *            Width of the board
@@ -42,24 +47,61 @@ class SoloLengthChallenge extends ChessStrategy {
     private StringField makeStage0(int width, int height) {
         ArrayList<String> directions = new ArrayList<>(width * height);
 
-        // bottom rows just instruct to go to top rows
+        // first row
+        for (int x = 0; x < width - 1; ++x) {
+            directions.add(RIGHT);
+        }
+        directions.add(UP);
+
+        // second row
+        directions.add(DOWN);
+        for (int x = 1; x < width; ++x) {
+            directions.add(LEFT);
+        }
+
         for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height - 2; ++y) {
-                directions.add(Move.Moves.UP);
+            for (int y = 2; y < height; ++y) {
+                directions.add(DOWN);
             }
         }
 
-        // pre-last row
-        for (int x = 0; x < width - 1; ++x) {
-            directions.add(Move.Moves.RIGHT);
-        }
-        directions.add(Move.Moves.UP);
+        return StringField.of(width, height, directions.toArray(new String[0]));
+    }
 
-        // last row
-        directions.add(Move.Moves.DOWN);
-        for (int x = 0; x < width - 1; ++x) {
-            directions.add(Move.Moves.LEFT);
+    /**
+     * Stage one. Eat every turn, fill the field
+     *
+     * @param width
+     *            Width of the board
+     * @param height
+     *            Height of the board
+     * @return Instructions on how to cover the field
+     */
+    private StringField makeStage1(int width, int height) {
+        ArrayList<String> directions = new ArrayList<>(width * height);
+
+        for (int y = 0; y < height; ++y) {
+            if (y % 2 == 0) {
+                for (int x = 0; x < width - 1; ++x) {
+                    directions.add(RIGHT);
+                }
+                directions.add(UP);
+            } else {
+                directions.add(UP);
+                for (int x = 1; x < width; ++x) {
+                    directions.add(LEFT);
+                }
+            }
         }
+
+        int lastMoveIndex;
+        if (height % 2 == 0) {
+            lastMoveIndex = directions.size() - 1;
+        } else {
+            lastMoveIndex = directions.size() + 1 - width;
+        }
+
+        directions.set(lastMoveIndex, DOWN);
 
         return StringField.of(width, height, directions.toArray(new String[0]));
     }
