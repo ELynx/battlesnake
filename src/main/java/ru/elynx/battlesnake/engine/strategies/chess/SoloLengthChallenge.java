5d7c6413f10a5095f943edbaee97ff3965657b52
ELynx <1621251+ELynx@users.noreply.github.com>
2021-03-16 18:47:51 +0300

    less luck OK

--- src/main/java/ru/elynx/battlesnake/engine/strategies/chess/SoloLengthChallenge.java
+++ src/main/java/ru/elynx/battlesnake/engine/strategies/chess/SoloLengthChallenge.java
@@ -4,14 +4,19 @@ import static ru.elynx.battlesnake.protocol.Move.Moves.*;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
+import ru.elynx.battlesnake.protocol.CoordsDto;
 import ru.elynx.battlesnake.protocol.GameStateDto;
 
 class SoloLengthChallenge extends ChessStrategy {
-    private boolean latch = false;
-    
+    private boolean latch1 = false;
+    private boolean latch2 = false;
+
     @Override
     protected int calculateStage(GameStateDto gameStateDto) {
-        if (latch)
+        if (latch2)
+            return 2;
+
+        if (latch1)
             return 1;
 
         // wait for length
@@ -22,8 +27,13 @@ class SoloLengthChallenge extends ChessStrategy {
         if (gameStateDto.getBoard().getFood().size() < 24)
             return 0;
 
-        // rush board
-        latch = true;
+        final CoordsDto head = gameStateDto.getYou().getHead();
+        if (head.getX() == 6 && head.getY() == 0) {
+            latch2 = true;
+            return 2;
+        }
+
+        latch1 = true;
         return 1;
     }
 
@@ -31,6 +41,9 @@ class SoloLengthChallenge extends ChessStrategy {
     protected StringField makeDirections(int stage, int width, int height) {
         if (stage == 1)
             return makeStage1(width, height);
+
+        if (stage == 2)
+            return makeStage2(width, height);
 
         // in any unknown situation, go loop
         return makeStage0(width, height);
@@ -70,7 +83,7 @@ class SoloLengthChallenge extends ChessStrategy {
     }
 
     /**
-     * Stage one. Eat every turn, fill the field.
+     * Stage one. Wait for escape.
      *
      * @param width
      *            7
@@ -83,10 +96,43 @@ class SoloLengthChallenge extends ChessStrategy {
             return null;
         }
 
-        String[] map6 = {UP, LEFT, LEFT, LEFT, LEFT, LEFT, LEFT};
-        String[] map5 = {RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, UP};
-        String[] map4 = map6;
+        String[] map6 = {DOWN, DOWN, DOWN, DOWN, DOWN, DOWN, DOWN};
+        String[] map5 = map6;
+        String[] map4 = map5;
         String[] map3 = {DOWN, LEFT, DOWN, LEFT, DOWN, LEFT, UP};
+        String[] map2 = {DOWN, UP, DOWN, UP, DOWN, RIGHT, UP};
+        String[] map1 = {DOWN, UP, LEFT, UP, LEFT, UP, LEFT};
+        String[] map0 = {RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, UP};
+
+        Stream<String> directions = Arrays.stream(map0);
+        directions = Stream.concat(directions, Arrays.stream(map1));
+        directions = Stream.concat(directions, Arrays.stream(map2));
+        directions = Stream.concat(directions, Arrays.stream(map3));
+        directions = Stream.concat(directions, Arrays.stream(map4));
+        directions = Stream.concat(directions, Arrays.stream(map5));
+        directions = Stream.concat(directions, Arrays.stream(map6));
+
+        return StringField.of(width, height, directions.toArray(String[]::new));
+    }
+
+    /**
+     * Stage two. Run.
+     *
+     * @param width
+     *            7
+     * @param height
+     *            7
+     * @return Instructions on how to cover the field.
+     */
+    private StringField makeStage2(int width, int height) {
+        if (width != 7 || height != 7) {
+            return null;
+        }
+
+        String[] map6 = {DOWN, LEFT, LEFT, LEFT, LEFT, LEFT, LEFT};
+        String[] map5 = {DOWN, LEFT, LEFT, LEFT, LEFT, LEFT, UP};
+        String[] map4 = {RIGHT, RIGHT, RIGHT, RIGHT, DOWN, UP, UP};
+        String[] map3 = {DOWN, LEFT, DOWN, LEFT, RIGHT, UP, UP};
         String[] map2 = {DOWN, UP, DOWN, UP, DOWN, RIGHT, UP};
         String[] map1 = {DOWN, UP, LEFT, UP, LEFT, UP, LEFT};
         String[] map0 = {RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, RIGHT, UP};
