7f2240725b5e1567872ba6ceb9e71d796abe0e59
ELynx <1621251+ELynx@users.noreply.github.com>
2021-03-27 01:08:57 +0300

    such c much copypaste

--- src/main/java/ru/elynx/battlesnake/engine/math/FreeSpaceMatrix.java
+++ src/main/java/ru/elynx/battlesnake/engine/math/FreeSpaceMatrix.java
@@ -1,15 +1,15 @@
 package ru.elynx.battlesnake.engine.math;
 
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import org.javatuples.Pair;
-
 public class FreeSpaceMatrix {
+    private final static int UNSET_VALUE = -1;
+    private final static int FILL_VALUE = -2;
+
     private final int width;
     private final int height;
     private final int length;
 
     private final int[] spaceValues;
+    private final int[] stack;
 
     protected FreeSpaceMatrix(int width, int height) {
         this.width = width;
@@ -17,6 +17,7 @@ public class FreeSpaceMatrix {
         this.length = this.width * this.height;
 
         this.spaceValues = new int[this.length];
+        this.stack = new int[this.length * 2];
     }
 
     public static FreeSpaceMatrix uninitializedMatrix(int width, int height) {
@@ -31,9 +32,107 @@ public class FreeSpaceMatrix {
 
     public void empty() {
         for (int i = 0; i < length; ++i) {
-            // by default all cells are uninitialized
-            spaceValues[i] = -1;
+            spaceValues[i] = UNSET_VALUE;
         }
+    }
+
+    private boolean inside(int x, int y) {
+        final int insideIndex = safeIndex(x, y);
+        if (insideIndex < 0) {
+            return false;
+        }
+        return spaceValues[insideIndex] == UNSET_VALUE;
+    }
+
+    private boolean insideAndSet(int x, int y) {
+        final int insideIndex = safeIndex(x, y);
+        if (insideIndex < 0) {
+            return false;
+        }
+        if (spaceValues[insideIndex] == UNSET_VALUE) {
+            spaceValues[insideIndex] = FILL_VALUE;
+            return true;
+        }
+        return false;
+    }
+
+    private int post() {
+        int filled = 0;
+        for (int i = 0; i < length; ++i) {
+            if (spaceValues[i] == FILL_VALUE)
+                ++filled;
+        }
+
+        for (int i = 0; i < length; ++i) {
+            if (spaceValues[i] == FILL_VALUE)
+                spaceValues[i] = filled;
+        }
+
+        return filled;
+    }
+
+    public int getSpaceImpl(int xIn, int yIn) {
+        stack[0] = xIn;
+        stack[1] = xIn;
+        stack[2] = yIn;
+        stack[3] = 1;
+
+        stack[4] = xIn;
+        stack[5] = xIn;
+        stack[6] = yIn - 1;
+        stack[7] = -1;
+
+        int stackPos = 8;
+        while (stackPos != 0) {
+            int x1 = stack[stackPos - 4];
+            int x2 = stack[stackPos - 3];
+            int y = stack[stackPos - 2];
+            int dy = stack[stackPos - 1];
+            stackPos -= 4;
+
+            int x = x1;
+
+            if (inside(x, y)) {
+                while (insideAndSet(x - 1, y)) {
+                    x -= 1;
+                }
+            }
+
+            if (x < x1) {
+                stack[stackPos] = x;
+                stack[stackPos + 1] = x1 - 1;
+                stack[stackPos + 2] = y - dy;
+                stack[stackPos + 3] = -dy;
+                stackPos += 4;
+            }
+
+            while (x1 < x2) {
+                while (insideAndSet(x1, y)) {
+                    x1 += 1;
+                }
+
+                stack[stackPos] = x;
+                stack[stackPos + 1] = x1 - 1;
+                stack[stackPos + 2] = y + dy;
+                stack[stackPos + 3] = dy;
+                stackPos += 4;
+
+                if (x1 - 1 > x2) {
+                    stack[stackPos] = x2 + 1;
+                    stack[stackPos + 1] = x1 - 1;
+                    stack[stackPos + 2] = y - dy;
+                    stack[stackPos + 3] = -dy;
+                }
+
+                while (x1 < x2 && !inside(x1, y)) {
+                    x1 += 1;
+                }
+
+                x = x1;
+            }
+        }
+
+        return post();
     }
 
     public int getSpace(int x, int y) {
@@ -46,37 +145,7 @@ public class FreeSpaceMatrix {
             return current;
         }
 
-        // inside by being -1
-        Predicate<Pair<Integer, Integer>> inside = pair -> {
-            final int insideIndex = safeIndex(pair.getValue0(), pair.getValue1());
-            if (insideIndex < 0) {
-                return false;
-            }
-            return spaceValues[insideIndex] == -1;
-        };
-
-        // set to -2 as marked
-        Consumer<Pair<Integer, Integer>> set = pair -> {
-            final int setIndex = unsafeIndex(pair.getValue0(), pair.getValue1());
-            spaceValues[setIndex] = -2;
-        };
-
-        // TODO algorithm
-
-        // count all -2s
-        int tagged = 0;
-        for (int i = 0; i < length; ++i) {
-            if (spaceValues[i] == -2)
-                ++tagged;
-        }
-
-        // write found space
-        for (int i = 0; i < length; ++i) {
-            if (spaceValues[i] == -2)
-                spaceValues[i] = tagged;
-        }
-
-        return tagged;
+        return getSpaceImpl(x, y);
     }
 
     public boolean setOccupied(int x, int y) {
