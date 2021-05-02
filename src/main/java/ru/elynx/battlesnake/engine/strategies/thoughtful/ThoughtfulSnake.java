1a915a318b6181d897b337d92a2122d451762406
ELynx <1621251+ELynx@users.noreply.github.com>
2021-05-02 20:03:14 +0300

    feed raw SnakePredictor to thoughful

--- src/main/java/ru/elynx/battlesnake/engine/strategies/thoughtful/ThoughtfulSnake.java
+++ src/main/java/ru/elynx/battlesnake/engine/strategies/thoughtful/ThoughtfulSnake.java
@@ -1,14 +1,28 @@
 package ru.elynx.battlesnake.engine.strategies.thoughtful;
 
+import static ru.elynx.battlesnake.protocol.Move.Moves.*;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.function.ToDoubleFunction;
+import org.javatuples.Triplet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.elynx.battlesnake.engine.IGameStrategy;
+import ru.elynx.battlesnake.engine.math.FreeSpaceMatrix;
 import ru.elynx.battlesnake.engine.predictor.GameStatePredictor;
+import ru.elynx.battlesnake.engine.predictor.IPredictorInformant;
+import ru.elynx.battlesnake.engine.predictor.SnakeMovePredictor;
+import ru.elynx.battlesnake.engine.strategies.CommonPatterns;
 import ru.elynx.battlesnake.protocol.BattlesnakeInfo;
+import ru.elynx.battlesnake.protocol.CoordsDto;
 import ru.elynx.battlesnake.protocol.Move;
 
-public class ThoughtfulSnake implements IGameStrategy {
+public class ThoughtfulSnake implements IGameStrategy, IPredictorInformant {
+    protected FreeSpaceMatrix freeSpaceMatrix;
+    protected SnakeMovePredictor predictor;
+
     protected ThoughtfulSnake() {
     }
 
@@ -18,23 +32,65 @@ public class ThoughtfulSnake implements IGameStrategy {
     }
 
     @Override
+    public void init(GameStatePredictor gameState) {
+        freeSpaceMatrix = FreeSpaceMatrix.uninitializedMatrix(gameState.getBoard().getWidth(),
+                gameState.getBoard().getHeight());
+        predictor = new SnakeMovePredictor(this);
+    }
+
+    @Override
     public Void processStart(GameStatePredictor gameState) {
-        throw new java.lang.UnsupportedOperationException("TODO");
+        return null;
     }
 
     @Override
     public Move processMove(GameStatePredictor gameState) {
-        throw new java.lang.UnsupportedOperationException("TODO");
+        freeSpaceMatrix.empty();
+        CommonPatterns.forSnakeBody(gameState,
+                coordsDto -> freeSpaceMatrix.setOccupied(coordsDto.getX(), coordsDto.getY()));
+
+        List<Triplet<Integer, Integer, Double>> predictions = predictor.predict(gameState.getYou(), gameState);
+
+        // repeat last
+        if (predictions.isEmpty()) {
+            return new Move();
+        }
+
+        predictions.sort(Comparator
+                .comparingDouble((ToDoubleFunction<Triplet<Integer, Integer, Double>>) Triplet::getValue2).reversed());
+
+        Triplet<Integer, Integer, Double> bestPrediction = predictions.get(0);
+        int px = bestPrediction.getValue0();
+        int py = bestPrediction.getValue1();
+
+        CoordsDto head = gameState.getYou().getHead();
+        int x = head.getX();
+        int y = head.getY();
+
+        int dx = px - x;
+        int dy = py - y;
+
+        if (dx > 0) {
+            return new Move(RIGHT);
+        } else if (dx < 0) {
+            return new Move(LEFT);
+        } else if (dy > 0) {
+            return new Move(UP);
+        } else if (dy < 0) {
+            return new Move(DOWN);
+        } else {
+            return new Move();
+        }
     }
 
     @Override
     public Void processEnd(GameStatePredictor gameState) {
-        throw new java.lang.UnsupportedOperationException("TODO");
+        return null;
     }
 
     @Override
-    public boolean isCombatant() {
-        return true;
+    public boolean isWalkable(int x, int y) {
+        return freeSpaceMatrix.isFree(x, y);
     }
 
     @Configuration
