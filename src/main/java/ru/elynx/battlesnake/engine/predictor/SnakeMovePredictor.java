503069f8e8afe96153b7c69e27fdb52d6b47c0d9
ELynx <1621251+ELynx@users.noreply.github.com>
2021-05-25 23:58:15 +0300

    Clear Code

--- src/main/java/ru/elynx/battlesnake/engine/predictor/SnakeMovePredictor.java
+++ src/main/java/ru/elynx/battlesnake/engine/predictor/SnakeMovePredictor.java
@@ -5,34 +5,18 @@ import java.util.List;
 import org.javatuples.Triplet;
 import ru.elynx.battlesnake.engine.predictor.implementation.ProbabilityMaker;
 import ru.elynx.battlesnake.engine.predictor.implementation.ScoreMaker;
+import ru.elynx.battlesnake.protocol.CoordsDto;
 import ru.elynx.battlesnake.protocol.SnakeDto;
 
 public class SnakeMovePredictor {
-    protected IPredictorInformant informant;
-    protected ScoreMaker scoreMaker;
-    protected ProbabilityMaker probabilityMaker;
+    private final IPredictorInformant informant;
+    private final ScoreMaker scoreMaker;
+    private final ProbabilityMaker probabilityMaker;
 
     public SnakeMovePredictor(IPredictorInformant informant) {
         this.informant = informant;
         this.scoreMaker = new ScoreMaker();
         this.probabilityMaker = new ProbabilityMaker();
-    }
-
-    protected void addScoredMoveIfWalkable(int x, int y) {
-        if (informant.isWalkable(x, y)) {
-            int score = scoreMaker.scoreMove(x, y);
-            probabilityMaker.add(x, y, score);
-        }
-    }
-
-    protected void addMoveIfWalkable(int x, int y) {
-        if (informant.isWalkable(x, y)) {
-            probabilityMaker.add(x, y);
-        }
-    }
-
-    protected void addMove(int x, int y) {
-        probabilityMaker.add(x, y);
     }
 
     public List<Triplet<Integer, Integer, Double>> predict(SnakeDto snake, GameStatePredictor gameState) {
@@ -41,35 +25,21 @@ public class SnakeMovePredictor {
             return Collections.emptyList();
         }
 
-        scoreMaker.reset(snake, gameState);
-        probabilityMaker.reset();
+        prepareInternals(snake, gameState);
 
-        // head this turn
-        final int x1 = snake.getHead().getX();
-        final int y1 = snake.getHead().getY();
+        // head position this turn
+        CoordsDto head = snake.getHead();
+        int x1 = head.getX();
+        int y1 = head.getY();
+
+        // head position last turn
+        CoordsDto firstBodySegment = getFirstBodySegment(snake);
+        int x0 = firstBodySegment.getX();
+        int y0 = firstBodySegment.getY();
 
         // there are cases, notably start, when body pieces can overlap
-        int x0;
-        int y0;
-
-        if (snake.getLength() == 1) {
-            x0 = x1;
-            y0 = y1;
-        } else {
-            // head last turn
-            x0 = snake.getBody().get(1).getX();
-            y0 = snake.getBody().get(1).getY();
-        }
-
         if (x1 == x0 && y1 == y0) {
-            // equal possibility to go anywhere
-            addScoredMoveIfWalkable(x1 - 1, y1);
-            addScoredMoveIfWalkable(x1, y1 + 1);
-            addScoredMoveIfWalkable(x1 + 1, y1);
-            addScoredMoveIfWalkable(x1, y1 + 1);
-
-            scoreMaker.freeReferences();
-            return probabilityMaker.make();
+            return getFourWayProbability(x1, y1);
         }
 
         // delta of this move
@@ -83,9 +53,7 @@ public class SnakeMovePredictor {
         if (snake.isTimedOut()) {
             // timed out snakes do not care for walk-ability
             addMove(xf, yf);
-
-            scoreMaker.freeReferences();
-            return probabilityMaker.make();
+            return makeProbabilities();
         }
 
         // magic of matrix multiplication
@@ -98,18 +66,81 @@ public class SnakeMovePredictor {
         final int xr = x1 + dy;
         final int yr = y1 - dx;
 
-        addScoredMoveIfWalkable(xf, yf);
-        addScoredMoveIfWalkable(xl, yl);
-        addScoredMoveIfWalkable(xr, yr);
+        // TODO generator for this
+        return getProbabilityOfXYArray(new int[]{xf, yf, xl, yl, xr, yr});
+    }
 
-        // if all choices are negatively bad
-        if (probabilityMaker.isEmpty()) {
-            // fill in undifferentiated
-            addMoveIfWalkable(xf, yf);
-            addMoveIfWalkable(xl, yl);
-            addMoveIfWalkable(xr, yr);
+    private List<Triplet<Integer, Integer, Double>> getFourWayProbability(int x, int y) {
+        // possibility to go anywhere
+        return getProbabilityOfXYArray(getFourDirections(x, y));
+    }
+
+    // TODO better type
+    private int[] getFourDirections(int x, int y) {
+        return new int[]{x - 1, y, x, y + 1, x + 1, y, x, y + 1};
+    }
+
+    private List<Triplet<Integer, Integer, Double>> getProbabilityOfXYArray(int[] items) {
+        for (int i = 0; i < items.length; i = i + 2) {
+            int x = items[i];
+            int y = items[i + 1];
+            addScoredMoveIfWalkable(x, y);
         }
 
+        if (noMovesAdded()) {
+            for (int i = 0; i < items.length; i = i + 2) {
+                int x = items[i];
+                int y = items[i + 1];
+                addMoveIfWalkable(x, y);
+            }
+        }
+
+        return makeProbabilities();
+    }
+
+    private void prepareInternals(SnakeDto snake, GameStatePredictor gameState) {
+        scoreMaker.reset(snake, gameState);
+        probabilityMaker.reset();
+    }
+
+    private CoordsDto getFirstBodySegment(SnakeDto snake) {
+        if (snake.getLength().equals(1)) {
+            return snake.getHead();
+        } else {
+            return snake.getBody().get(1);
+        }
+    }
+
+    private boolean noMovesAdded() {
+        return probabilityMaker.isEmpty();
+    }
+
+    private void addScoredMoveIfWalkable(int x, int y) {
+        if (informant.isWalkable(x, y)) {
+            addScoredMove(x, y);
+        }
+    }
+
+    private void addScoredMove(int x, int y) {
+        int score = scoreMaker.scoreMove(x, y);
+        addMove(x, y, score);
+    }
+
+    private void addMove(int x, int y, int score) {
+        probabilityMaker.add(x, y, score);
+    }
+
+    private void addMoveIfWalkable(int x, int y) {
+        if (informant.isWalkable(x, y)) {
+            addMove(x, y);
+        }
+    }
+
+    private void addMove(int x, int y) {
+        probabilityMaker.add(x, y);
+    }
+
+    private List<Triplet<Integer, Integer, Double>> makeProbabilities() {
         scoreMaker.freeReferences();
         return probabilityMaker.make();
     }
