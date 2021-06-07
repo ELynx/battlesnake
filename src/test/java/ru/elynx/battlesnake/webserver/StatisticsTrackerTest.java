b18e7e48d88e661e7cfaf419ec908a396608e40f
ELynx <1621251+ELynx@users.noreply.github.com>
2021-06-07 19:01:20 +0300

    work on tests

--- src/test/java/ru/elynx/battlesnake/webserver/StatisticsTrackerTest.java
+++ src/test/java/ru/elynx/battlesnake/webserver/StatisticsTrackerTest.java
@@ -1,39 +1,57 @@
 package ru.elynx.battlesnake.webserver;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.elynx.battlesnake.testbuilder.EntityBuilder;
 
+@SpringBootTest
 @Tag("Internals")
 class StatisticsTrackerTest {
     @Test
-    void test_root() {
-        fail("Test not implemented");
+    void test_root(@Autowired StatisticsTracker tested) {
+        assertDoesNotThrow(() -> tested.root("Test"));
     }
 
     @Test
-    void test_start() {
-        fail("Test not implemented");
+    void test_start(@Autowired StatisticsTracker tested) {
+        assertDoesNotThrow(() -> tested.start(EntityBuilder.hazardPredictor().getGameState()));
     }
 
     @Test
-    void test_move() {
-        fail("Test not implemented");
+    void test_move(@Autowired StatisticsTracker tested) {
+        assertDoesNotThrow(() -> tested.move(EntityBuilder.hazardPredictor().getGameState()));
     }
 
     @Test
-    void test_end() {
-        fail("Test not implemented");
+    void test_end(@Autowired StatisticsTracker tested) {
+        assertDoesNotThrow(() -> tested.end(EntityBuilder.hazardPredictor().getGameState()));
     }
 
     @Test
-    void ping() {
-        fail("Test not implemented");
+    void ping(@Autowired StatisticsTracker tested) {
+        assertDoesNotThrow(tested::ping);
     }
 
     @Test
-    void test_get_pings() {
-        fail("Test not implemented");
+    void test_get_pings(@Autowired StatisticsTracker tested) {
+        assertDoesNotThrow(() -> {
+            long tmp = tested.getPings();
+            assertThat(tmp, is(greaterThanOrEqualTo(0L)));
+        });
+
+        long before = tested.getPings();
+        tested.ping();
+        tested.ping();
+        tested.ping();
+        long after = tested.getPings();
+
+        assertEquals(before + 3, after);
     }
 }
