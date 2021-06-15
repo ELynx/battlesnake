fdbf24ee0a1614a3b27d05083e774f0d2dcfa07b
ELynx <1621251+ELynx@users.noreply.github.com>
2021-06-15 19:45:39 +0300

    clear code

--- src/main/java/ru/elynx/battlesnake/webserver/UtilityController.java
+++ src/main/java/ru/elynx/battlesnake/webserver/UtilityController.java
@@ -25,6 +25,11 @@ public class UtilityController {
         logger.info("Processing status call");
         statisticsTracker.ping();
 
+        String status = makeStatus();
+        return ResponseEntity.ok(status);
+    }
+
+    private String makeStatus() {
         long statusPings = statisticsTracker.getPings();
 
         long mb = 1024L * 1024L;
@@ -33,9 +38,7 @@ public class UtilityController {
         long freeMemory = runtime.freeMemory() / mb;
         long totalMemory = runtime.totalMemory() / mb;
 
-        String response = String.format("Status pings %s%nFree memory, MB %s%nTotal memory, MB %s", statusPings,
-                freeMemory, totalMemory);
-
-        return ResponseEntity.ok(response);
+        return String.format("Status pings %s%nFree memory, MB %s%nTotal memory, MB %s", statusPings, freeMemory,
+                totalMemory);
     }
 }
