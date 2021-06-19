7d2521dc67518a764eee1075a52fe83666fafa01
ELynx <1621251+ELynx@users.noreply.github.com>
2021-06-19 22:12:56 +0300

    more failed tests

--- src/test/java/ru/elynx/battlesnake/engine/advancer/GameStateAdvancerTest.java
+++ src/test/java/ru/elynx/battlesnake/engine/advancer/GameStateAdvancerTest.java
@@ -14,6 +14,7 @@ import ru.elynx.battlesnake.testbuilder.EntityBuilder;
 
 class GameStateAdvancerTest {
     BiFunction<Snake, GameState, MoveCommand> moveRight = (Snake snake, GameState gameState) -> MoveCommand.RIGHT;
+    BiFunction<Snake, GameState, MoveCommand> moveDown = (Snake snake, GameState gameState) -> MoveCommand.DOWN;
 
     @Test
     void test_constants() {
@@ -57,17 +58,56 @@ class GameStateAdvancerTest {
         HazardPredictor entity1 = new AsciiToGameState("" + //
                 "yy_\n" + //
                 "_Y_\n" + //
-                "___\n").setStartSnakeSize(1).build();
+                "___\n").build();
         GameState from = entity1.getGameState();
 
         HazardPredictor entity2 = new AsciiToGameState("" + //
                 "_y_\n" + //
                 "_yY\n" + //
-                "___\n").setStartSnakeSize(1).build();
+                "___\n").build();
         GameState expected = entity2.getGameState();
 
         GameState to = GameStateAdvancer.advance(from, moveRight);
 
         assertEquals(expected.getBoard(), to.getBoard());
+    }
+
+    @Test
+    void test_snake_health_decrease() {
+        HazardPredictor entity1 = EntityBuilder.hazardPredictor();
+        GameState from = entity1.getGameState();
+        GameState to = GameStateAdvancer.advance(from, moveRight);
+
+        assertEquals(from.getYou().getHealth() - 1, to.getYou().getHealth());
+    }
+
+    @Test
+    void test_snake_eat_food() {
+        HazardPredictor entity1 = new AsciiToGameState("" + //
+                "_yy\n" + //
+                "_Y_\n" + //
+                "_0_\n").setTurn(1).setHealth("Y", 42).build();
+        GameState turn1 = entity1.getGameState();
+
+        HazardPredictor entity2 = new AsciiToGameState("" + //
+                "_y_\n" + //
+                "_y_\n" + //
+                "_Y_\n").setTurn(2).setHealth("Y", 100).build();
+        GameState turn2 = entity2.getGameState();
+
+        HazardPredictor entity3 = new AsciiToGameState("" + //
+                "_y_\n" + //
+                "_y_\n" + //
+                "_yY\n").setTurn(3).setHealth("Y", 99).build();
+        GameState turn3 = entity3.getGameState();
+
+        GameState advance2 = GameStateAdvancer.advance(turn1, moveDown);
+
+        GameState advance3fromAdvance2 = GameStateAdvancer.advance(advance2, moveRight);
+        GameState advance3fromTurn2 = GameStateAdvancer.advance(turn2, moveRight);
+
+        assertEquals(turn2, advance2);
+        assertEquals(turn3, advance3fromAdvance2);
+        assertEquals(turn3, advance3fromTurn2);
     }
 }
