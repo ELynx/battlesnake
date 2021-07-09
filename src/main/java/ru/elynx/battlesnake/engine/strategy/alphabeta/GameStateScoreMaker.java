ede9dda370c793043ef98ad4545a264d4b42a582
ELynx <1621251+ELynx@users.noreply.github.com>
2021-07-09 18:25:13 +0300

    basic idea

--- src/main/java/ru/elynx/battlesnake/engine/strategy/alphabeta/GameStateScoreMaker.java
+++ src/main/java/ru/elynx/battlesnake/engine/strategy/alphabeta/GameStateScoreMaker.java
@@ -10,26 +10,50 @@ public class GameStateScoreMaker {
     private static final Boolean TERMINATE = Boolean.TRUE;
     private static final Boolean CONTINUE = Boolean.FALSE;
 
-    private static final int LOSE_SCORE = -100;
-    private static final int WIN_SCORE = 100;
-
     public static Pair<Boolean, Integer> makeYouScore(GameState state0, GameState state1) {
         return makeScore(state0.getYou(), state0, state1);
     }
 
     public static Pair<Boolean, Integer> makeScore(Snake snake, GameState state0, GameState state1) {
+        int scoreLimit = calculateScoreLimit(state0);
+
         if (state1.isEliminated(snake)) {
-            return new Pair<>(TERMINATE, LOSE_SCORE);
+            return new Pair<>(TERMINATE, -scoreLimit);
         }
 
         if (oneSnakeLeft(state0, state1)) {
-            return new Pair<>(TERMINATE, WIN_SCORE);
+            return new Pair<>(TERMINATE, scoreLimit);
         }
 
-        return new Pair<>(CONTINUE, 1);
+        int score = stateScore(snake, state1) - stateScore(snake, state0);
+        return new Pair<>(CONTINUE, score);
+    }
+
+    private static int calculateScoreLimit(GameState gameState) {
+        return gameState.getBoard().getDimensions().area() * Snake.getMaxHealth();
     }
 
     private static boolean oneSnakeLeft(GameState gameState0, GameState gameState1) {
         return gameState0.getBoard().getSnakes().size() > 1 && gameState1.getBoard().getSnakes().size() == 1;
+    }
+
+    private int stateScore(Snake snake, GameState gameState) {
+        int score = 0;
+
+        for (Snake someSnake : gameState.getBoard().getSnakes()) {
+            int power = snakePower(someSnake);
+
+            if (someSnake.getId().equals(snake.getId())) {
+                score += power;
+            } else {
+                score -= power;
+            }
+        }
+
+        return score;
+    }
+
+    private int snakePower(Snake snake) {
+        return snake.getLength() * snake.getHealth();
     }
 }
